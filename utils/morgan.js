7cbc05caf0767d62b3e3d0bb75be78ef10ddecac
fathonialimukti <fathonialim2@gmail.com>
2022-12-12 17:40:54 +0700

    update

--- utils/morgan.js
+++ utils/morgan.js
@@ -2,8 +2,7 @@ import morgan from "morgan"
 import logger from "./logger.js"
 
 const stream = {
-    // Use the http severity
-    write: ( message ) => logger.http( message ),
+    write: ( message ) => logger.https( message ),
 }
 
 const skip = () => {
@@ -12,13 +11,7 @@ const skip = () => {
 }
 
 const morganMiddleware = morgan(
-    // Define message format string (this is the default one).
-    // The message format is made from tokens, and each token is
-    // defined inside the Morgan library.
-    // You can create your custom token to show what do you want from a request.
     ":remote-addr :method :url :status :res[content-length] - :response-time ms",
-    // Options: in this case, I overwrote the stream and the skip logic.
-    // See the methods above.
     { stream, skip }
 )
 
